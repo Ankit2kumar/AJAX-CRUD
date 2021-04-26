58d56bd7a8022c9d4f085f82b098947e896dd891
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-04-26 22:54:26 +0200

    AJAX-CRUD...(M3-D10)

---
+++ backOffice.js
@@ -0,0 +1,41 @@
+const addNewData = document.querySelector('#addNewData');
+
+function enableform() {
+	myForm.classList.remove('d-none');
+}
+
+const getData = () => {
+	const movieName = document.querySelector('#InputMovieName').value;
+	const movieDesc = document.querySelector('#InputMovieDesc').value;
+	const moviePrice = document.querySelector('#InputMoviePrice').value;
+	const movieImgUrl = document.querySelector('#InputMovieUrl').value;
+	const movieCate = document.querySelector('#InputMovieCate').value;
+	return (Data = {
+		name: movieName,
+		description: movieDesc,
+		price: moviePrice,
+		category: movieCate,
+		imageUrl: movieImgUrl,
+	});
+};
+
+const submit = document.querySelector('#submit');
+
+const addMovies = async (event) => {
+	event.preventDefault();
+	const resp = await fetch(
+		'https://striveschool-api.herokuapp.com/api/movies/',
+		{
+			method: 'POST',
+			headers: {
+				Authorization:
+					'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDZhMGIxZjBmYjAwMTVkOTE2ZmIiLCJpYXQiOjE2MTkwMDMwNDEsImV4cCI6MTYyMDIxMjY0MX0.BUjunefP1cqV1viZCRo9wZf_AWri7aeJAYeJILezuJA',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(getData()),
+		}
+	);
+	if (resp.ok) {
+		alert('Data has been successfully addedd!!');
+	}
+};
